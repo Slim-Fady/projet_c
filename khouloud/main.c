dcf7af431fc10b61d9c8fd18c1fd75e4a39f2c92
khouloudbelhadj <115471689+khouloudbelhadj@users.noreply.github.com>
2022-11-26 16:24:47 +0100

    Add files via upload

--- khouloud/main.c
+++ khouloud/main.c
@@ -7,34 +7,35 @@ int main()
 reclamation r1={111,"Belhadj", "Khouloud" , "khouloud.belhadj@esprit.tn","Problème"},
 r2={222,"Guemri", "Achraf" , "achraf.guemri@esprit.tn","Problème"},
 r3={333,"Asma", "Belhsan" , "asma.belhsan@esprit.tn","Problème"};
+    
     int x;
-   
     x=ajouter("reclamation.txt", r1);
     if(x==1)
-        printf("\najout d'une  reclamation  avec succés");
-    else printf("\nechec ajout");
+        printf("\n 1/Ajout d'une  réclamation  avec succés");
+    else printf("\n Echec ajout");
 
-    x=ajouter("reclamation.txt", r2);
-    if(x==1)
-        printf("\najout d'une reclamation  avec succés");
-    else printf("\nechec ajout");
+    // x=ajouter("reclamation.txt", r2);
+   // if(x==1)
+    //    printf("\n Ajout d'une réclamation  avec succés");
+   // else printf("\n Echec ajout");
 
-    x=ajouter("reclamation.txt", r3);
-    if(x==1)
-        printf("\najout d'une reclamation  avec succés");
-    else printf("\nechec ajout");
+   // x=ajouter("reclamation.txt", r3);
+   // if(x==1)
+      //  printf("\n Ajout d'une réclamation  avec succés");
+   // else printf("\n Echec ajout");
 
     x=modifier("reclamation.txt",111,r2 );
     if(x==1)
-        printf("\nModification de la  reclamation  avec succés");
-    else printf("\nechec Modification");
+        printf("\n 2/Modification de la  réclamation  avec succés");
+    else printf("\n Echec Modification");
 
     x=supprimer("reclamation.txt",222 );
     if(x==1)
-        printf("\nSuppression de la reclamation  avec succés");
-    else printf("\nechec Suppression");
-    r3=chercher("reclamation.txt",333 );
-    if(r3.id==-1)
+        printf("\n 3/Suppression de la réclamation  avec succés");
+    else printf("\n Echec Suppression");
+    
+    r1=chercher("reclamation.txt",111 );
+    if(r1.id==-1)
         printf("introuvable");
     else
         printf("\n existe ");
--- khouloud/reclamation.h
+++ khouloud/reclamation.h
@@ -6,12 +6,12 @@ typedef struct reclamation
     int id ;
     char nom [20];
     char prenom[20];
-    char email[20];
-    char votre_message[20];
+    char email[100];
+    char votre_message[1000];
 } reclamation;
 
-int ajouter(char filename[20],struct reclamation  );
-int modifier( char *filename, int id ,struct reclamation  );
+int ajouter(char filename[20],struct reclamation );
+int modifier( char *filename, int id ,struct reclamation );
 int supprimer(char *filename, int id );
 reclamation  chercher(char *filename, int id);
 
